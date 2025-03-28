9a64c48865ed056b9d7b1861e65d6afce9163dac
KUT0K1 <95503527+KUT0K1@users.noreply.github.com>
2025-03-28 23:43:22 +0100

    Update entrypoint.sh

--- serverlist-server/entrypoint.sh
+++ serverlist-server/entrypoint.sh
@@ -1,19 +1,18 @@
 #!/bin/sh
 
-PARAMS=""
-
-# Set default value for nodb unless explicitly set to false
+# Set default value for nodb if it not set
 NODB=${nodb:-true}
-if [ "$NODB" = "true" ]; then
-  PARAMS="${PARAMS} nodb=true"
-else
-  PARAMS="${PARAMS} nodb=false"
-fi
+PARAMS="nodb=$NODB"
 
 # Add additional parameters only if they are set
 [ -n "$USER_SERVERS" ] && PARAMS="${PARAMS} user_servers=${USER_SERVERS}"
 [ -n "$FEATURED_SERVERS" ] && PARAMS="${PARAMS} featured_servers=${FEATURED_SERVERS}"
 [ -n "$CUSTOM_SERVERS" ] && PARAMS="${PARAMS} custom_servers=${CUSTOM_SERVERS}"
+
+# If CMD arguments were passed, add them to PARAMS
+if [ "$#" -gt 0 ]; then
+  PARAMS="$PARAMS $@"
+fi
 
 # Start the application with the dynamic parameters
 exec java -jar BedrockConnect-1.0-SNAPSHOT.jar $PARAMS
